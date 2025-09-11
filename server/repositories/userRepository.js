14efbf1e289e9b5d247edfd8151dfbb998ba0650
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-11 21:31:03 +0800

    feature: repository
    add findById method excluding password

--- server/repositories/userRepository.js
+++ server/repositories/userRepository.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
 
 exports.findByEmail = (email) => User.findOne({ email });
+exports.findById = (id) => User.findById(id).select("-password")
 exports.createUser = (data) => User.create(data);
