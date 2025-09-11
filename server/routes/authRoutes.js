1234bcc94d0b32c436ca9aae1742f7510d47434c
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-11 21:32:07 +0800

    feautre: routes
    add refresh token and logout routes with authentication

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const {
+  register,
+  login,
+  refreshToken,
+  logout,
+} = require("../controllers/authController");
+const { authenticateToken } = require("../middleware/verifyToken");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/refresh", refreshToken);
+router.post("/logout",authenticateToken, logout);
 
 module.exports = router;
