69933373565cd9930f45127fefce75b709146241
dyaaah <95646179+Liebe3@users.noreply.github.com>
2025-09-14 20:08:38 +0800

    feature: route
    add protected routes for user profile and admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import AdminDashboard from "./admin/AdminDashboard";
 import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserProfilePage from "./pages/UserProfilePage";
 
 function App() {
   return (
@@ -11,6 +14,25 @@ function App() {
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute role="user">
+              <UserProfilePage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
